736b258073e5d816b9018668e9a32f4dd91290f3
Gonzavitola <101563291+Gonzavitola@users.noreply.github.com>
2022-05-20 14:18:50 -0300

    ex5 guia2

---
+++ Assets/Prefabcloner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Prefabcloner : MonoBehaviour
+{
+    public GameObject prefab;
+    public Text InputCloneAmount;
+    public int counter;
+    public Text displayClonesLeft;
+    // Start is called before the first frame update
+    void Start()
+    {
+        counter = 0;
+        displayClonesLeft.text = "?";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void ClonePrefab()
+    {
+        if (InputCloneAmount.text != "")
+        {
+            int cloneAmount = int.Parse(InputCloneAmount.text);
+
+            if (counter < cloneAmount)
+            {
+                Instantiate(prefab);
+                counter++;
+                displayClonesLeft.text = (cloneAmount - counter).ToString();
+            }
+            else
+            {
+                displayClonesLeft.text = "ERROR,ingrese un valor";
+            }
+        }
+    }
+}
